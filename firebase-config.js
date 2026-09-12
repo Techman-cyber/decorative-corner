04c18befb0acaef676a6a818e969aacf79602a0a
Chanakyaaa <chanakyasahni8@gmail.com>
2026-09-12 17:26:28 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -31,3 +31,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+export const db = getFirestore(app);
